1d76408ee960853b007fb7ffaf0ddcd01197bd16
TheLuge0 <151455192+TheLuge0@users.noreply.github.com>
2024-02-26 08:08:26 +0100

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -6,3 +6,9 @@ DATA_FILE = os.path.join(CUR_DIR, 'data')
 SYS_FILE = os.path.join(CUR_DIR, 'system')
 path_data = Path(DATA_FILE)
 path_system = Path(SYS_FILE)
+
+if not path_data.exists():
+    path_data.mkdir()
+
+if not path_system.exists():
+    path_system.mkdir()
